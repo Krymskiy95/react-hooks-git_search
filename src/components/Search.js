7147cd30db2652acb2cf544445eabfd22788a931
suprunov <rocknroll2418@gmail.com>
2021-07-16 18:23:44 +0300

    react-hook-git

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,23 @@
 import React, {useContext, useState} from "react";
 import {AlertContext} from "../context/alert/alertContext";
+import {GithubContext} from "../context/github/githubContext";
 
 export const Search = () => {
     const [value, setValue] = useState('')
-    const {show} = useContext(AlertContext)
+    const alert = useContext(AlertContext)
+    const github = useContext(GithubContext)
 
     const onSubmit = event => {
         if (event.key !== 'Enter') {
            return
         }
+        github.clearUsers()
+
         if (value.trim()) {
-            console.log('sdsd')
+            alert.hide()
+            github.search(value.trim())
         } else {
-            show('dsdsds')
+            alert.show('Ну привет')
         }
     }
 
